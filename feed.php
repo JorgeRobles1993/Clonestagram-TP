cf5b2ae72afe986c16cab9b5cec45f53c044d2e4
Robin Patissier <robinpatissier@gmail.com>
2024-01-25 16:36:01 +0100

    push fin journée 25/01

--- feed.php
+++ feed.php
@@ -15,8 +15,8 @@ $photofeed = $preparedRequestPhotoFeed->fetchAll(PDO::FETCH_ASSOC);
 <div class="row">
 
     <div class="border border-right border-2 col-2">
+        <nav class="stickynavbar navbar-nav ms-3">
         <h1 class="textlogo fs-2 ms-3">Clonestagram</h1>
-        <nav class="navbar-nav ms-3">
             <ul class="nav navbar-nav">
                 <li class="nav-item">
                  <a class="nav-link" href="./feed.php"><i class="fa-solid fa-house fa-xl fa-xl m-3" style="color: #000000;"></i> Accueil </a>
@@ -100,7 +100,7 @@ $photofeed = $preparedRequestPhotoFeed->fetchAll(PDO::FETCH_ASSOC);
                 <div class="upload">
     <h1 class="browse">Sélectionnez un fichier :</h1>
     <input type="file" id="file" class="input-file" name="photofeed" multiple>
-    <span id="file-select-button" class="browse button black">Sélectionnez des fichiers</span>
+    <span id="file-select-button" class="browse button black"></span>
     <button class="btn btn-primary" type="submit">Envoyer photo</button>
 </div>
                 </form>
