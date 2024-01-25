d7b515d31191d08ca4ecc378a1ebb9b23c403b83
Jorge Robles <jorgejoanrobles@gmail.com>
2024-01-25 11:38:19 +0100

    jorges push midi 25/01

--- process/process_photofeed.php
+++ process/process_photofeed.php
@@ -1,33 +1,30 @@
 <?php
 
 session_start();
+date_default_timezone_set('Europe/Paris');
 require_once '../config/connexion.php';
+
+
+$uploads = "../images/uploads";
+$tmp_name = $_FILES['photofeed']['tmp_name'];
+$name = $_FILES['photofeed']['name'];
+$addpict = move_uploaded_file($tmp_name, $uploads . '/' .$name );
+$_SESSION['photofeed'] = $name;
+$user_id = $_SESSION['id'];
 
 
 
 
 $preparedRequestAddphotofeed = $connexion->prepare( 
-    "INSERT INTO photos (`id`, `user_id`, `photo`) VALUES (?, ?, ?)"
+    "INSERT INTO photo (`user_id`, `photo`, `created_at`) VALUES (?, ?, ?)"
 );
 
 $preparedRequestAddphotofeed->execute([
-    $_SESSION['id'],
+    $user_id,
+    $name,
+    date("d-m-y h:i:s")
 
-
-])
-
-header('Location: ../profil.php');
-
-
-
-
-$preparedRequestUpdateUser = $connexion->prepare(
-    "UPDATE user SET profilephoto = ? WHERE id = ?"
-);
-// Execute la requete pour inserer le user 
-$preparedRequestUpdateUser->execute([
-    $name ?? null,
-    $_SESSION['id']
 ]);
 
-header('Location: ../profil.php');
+header('Location: ../feed.php');
+
